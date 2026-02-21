2723bcfbc43cba4a96d4106499fbfe0b01cdbfa1
sarthak <sarthak.shinde2006@gmail.com>
2026-02-21 11:15:43 +0530

    remember something that helps

--- DSA_Programs/sum_of_digit_base_k.java
+++ DSA_Programs/sum_of_digit_base_k.java
@@ -1,5 +1,7 @@
 public int sumofdigit(int n,int k){
     String result=Integer.parseInt(n,k);   //This converts to any base remember..
+    //one more you should know about this if the n is given in integer then do this
+    //String h=Integer.toString(n);    //remember this also
     int y=Integer.parseInt(result);
     while(y!=0){
         int r=y%10;
