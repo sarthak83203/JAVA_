ed532db3f1a0f4ab704ee8061c1268797a0a6a6c
sarthak <sarthak.shinde2006@gmail.com>
2026-02-28 12:08:44 +0530

    Daily challenge nice question

---
+++ DSA_Programs/dailychalenge.java
@@ -0,0 +1,16 @@
+public int concatenatedBinary(int n){
+    String str="";
+    int mod=1000000007;
+    for(int i=1;i<=3;i++){
+        str=str+Integer.toBinaryString(i);
+        long t=Long.parseLong(str,2);
+        val=val%mod;
+        str=Long.toBinaryString(val);
+    }
+    return (int)Long.parseLong(str,2);   //this is binary to decimal 
+
+}
+
+//now in this question or any other question 
+//1)convert to BinaryString
+//2)Then take its decimal
