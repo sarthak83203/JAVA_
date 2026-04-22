d5fd80ac64467389a425f5d08ff202932d859396
sarthak <sarthak.shinde2006@gmail.com>
2026-04-22 09:52:33 +0530

    first letter twice

---
+++ DSA_Programs/first_letter_twice.java
@@ -0,0 +1,11 @@
+  public char repeatedCharacter(String s) {
+        boolean seen[]=new boolean[26];
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(seen[ch-'a']){
+                return ch;
+            }
+            seen[ch-'a']=true;
+        }
+        return ' ';
+    }
