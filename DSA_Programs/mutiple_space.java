f20b6e781dfe092ddc75c02bc968a3069c47e938
sarthak <sarthak.shinde2006@gmail.com>
2026-05-25 08:15:56 +0530

    Info about \\s+

---
+++ DSA_Programs/mutiple_space.java
@@ -0,0 +1,9 @@
+public int segments(String s){
+    s=s.trim();  //this removes the space of first and end
+    if(s.length()==0){
+        return 0;
+    }
+    String i[]=s.split("\\s+");  //  "\\s" means whitespace + means one or more
+    return i.length;
+
+}
