0121819a08a55fa0ced7304a7b20eee03e77357b
sarthak83203 <sarthak.shinde2006@gmail.com>
2026-07-24 22:44:51 +0530

    forgot about this method

---
+++ DSA_Programs/Putting_ele_at_posi.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Solution {
+    public int[] createTargetArray(int[] nums, int[] index) {
+        int target[]=new int[nums.length];
+        ArrayList<Integer> list=new ArrayList<>();
+        for(int i=0;i<nums.length;i++){
+            list.add(index[i],nums[i]);//putting element at the position directly
+        }
+        for(int i=0;i<list.size();i++){
+            target[i]=list.get(i);
+        }
+        return target;
+        
+    }
+}
