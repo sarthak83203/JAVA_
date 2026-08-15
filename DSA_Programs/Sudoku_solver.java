6981b4457a3fbab56831735cde46525d24d39817
sarthak83203 <sarthak.shinde2006@gmail.com>
2026-08-15 11:41:01 +0530

    I should think this method

---
+++ DSA_Programs/Sudoku_solver.java
@@ -0,0 +1,25 @@
+public class Sudoku_solver {
+    public static boolean SudokuSolver(int sudoku[][],int row,int col){
+        //base case
+
+
+        //recursion
+        for(int digit=0;digit<=9;digit++){
+            
+        }
+
+    }
+    public static void main(String args[]){
+        int sudoku[][]={{0,0,8,0,0,0,0,0,0},
+                       {4,9,0,1,5,7,0,0,2},
+                       {0,0,3,0,0,4,1,9,0},
+                       {1,8,5,0,6,0,0,2,0},
+                       {0,0,0,0,2,0,0,6,0},
+                       {9,6,0,4,0,5,3,0,0},
+                       {0,3,0,0,7,2,0,0,4},
+                       {0,4,9,0,3,0,0,5,7},
+                       {8,2,7,0,0,9,0,1,3}};
+
+    }
+    
+}
