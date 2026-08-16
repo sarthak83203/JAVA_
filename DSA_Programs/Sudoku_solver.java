a471d3ae7aed9391b77c825e9d336bbdea91a2ea
sarthak83203 <sarthak.shinde2006@gmail.com>
2026-08-16 16:09:43 +0530

    Sudoku solved

--- DSA_Programs/Sudoku_solver.java
+++ DSA_Programs/Sudoku_solver.java
@@ -1,13 +1,74 @@
 public class Sudoku_solver {
+    public static boolean isSafe(int sudoku[][],int row,int col,int digit){
+
+        //coloum
+        for(int i=0;i<=8;i++){
+            if(sudoku[i][col]==digit){
+                return false;
+            }
+        }
+
+        //row
+        for(int j=0;j<=8;j++){
+            if(sudoku[row][j]==digit){
+                return false;
+            }
+        }
+
+        //grid
+        //this is formula should remember for the shortcut
+        //isme ab hum grid me travel karege
+        int sr=(row/3)*3;  //isme (row/3) represent karta he ki konsa part hoga like 0,1,2 and (*3) represent the starting point of the grid
+        int sc=(col/3) *3; //same for this also
+        for(int i=sr;i<sr+3;i++){
+            for(int j=sc;j<sc+3;j++){
+                if(sudoku[i][j]==digit){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+
+
     public static boolean SudokuSolver(int sudoku[][],int row,int col){
         //base case
+        if(row==9){
+            return true;
+        }
 
 
         //recursion
-        for(int digit=0;digit<=9;digit++){
-            
+        int nextRow=row;
+        int nextCol=col+1;
+        if(col+1==9){
+            nextRow=row+1;
+            nextCol=0;
         }
+        if(sudoku[row][col]!=0){
+            return SudokuSolver(sudoku, nextRow, nextCol);
+        }
+        for(int digit=0;digit<=9;digit++){
+            if(isSafe(sudoku,row,col,digit)){
+                sudoku[row][col]=digit;
+                if(SudokuSolver(sudoku, nextRow, nextCol)){ //solution exits
+                    return true;
+                }
+                sudoku[row][col]=0;
+            }  
+        }
+        return false;
 
+    }
+
+    public static void printSudoku(int sudoku[][]){
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                System.out.print(sudoku[i][j]+" ");
+            }
+            System.out.println();
+        }
     }
     public static void main(String args[]){
         int sudoku[][]={{0,0,8,0,0,0,0,0,0},
@@ -19,6 +80,14 @@ public class Sudoku_solver {
                        {0,3,0,0,7,2,0,0,4},
                        {0,4,9,0,3,0,0,5,7},
                        {8,2,7,0,0,9,0,1,3}};
+
+
+                if(SudokuSolver(sudoku, 0, 0)){
+                    System.out.println("Solution exists");
+                    printSudoku(sudoku);
+                }else{
+                    System.out.println("Solution does not exists...");
+                }
 
     }
     
