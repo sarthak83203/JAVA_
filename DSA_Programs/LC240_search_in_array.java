7de11053585f5888f35e0e5cdb2f513a1f40fe06
sarthak83203 <sarthak.shinde2006@gmail.com>
2026-08-26 19:27:49 +0530

    Search in 2D matrix- II

--- DSA_Programs/LC240_search_in_array.java
+++ DSA_Programs/LC240_search_in_array.java
@@ -1,5 +1,7 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
+        //isme vohi approach use kiya he like right most right coner and 
+        //and leftmost down corner inmese koi bhi choose karu to answer aa jayega easily
         int row=0;
         int col=matrix[0].length-1;
         while(row<matrix.length && col>=0){
