d4c67fc424a37cb59ac7522956e34a17830f11de
sarthak83203 <sarthak.shinde2006@gmail.com>
2026-08-29 22:15:08 +0530

    IsCycle Exists

--- DSA_Programs/LC141_iscycle.java
+++ DSA_Programs/LC141_iscycle.java
@@ -12,16 +12,16 @@
  */
 class Solution {
     public boolean hasCycle(ListNode head) {
-        ListNode slow=head;
-        ListNode fast=head;
+        ListNode slow=head;//slow pointer 
+        ListNode fast=head;//fast pointer
         while(fast!=null && fast.next!=null){
             slow=slow.next;//+1
             fast=fast.next.next;
-            if(slow==fast){
+            if(slow==fast){//agar cycle exist hogi to ye dono ek jagah milege tab true 
                 return true;
             }
         }
-        return false;
+        return false;//nahi to false
         
     }
 }
