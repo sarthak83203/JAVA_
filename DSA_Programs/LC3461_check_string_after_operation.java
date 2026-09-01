aaeff49687dda6d5c0c440eaab8bf485f81774bf
sarthak83203 <sarthak.shinde2006@gmail.com>
2026-09-01 10:06:11 +0530

    Thoda socho while loop will help to solve this problem very quickly

--- DSA_Programs/LC3461_check_string_after_operation.java
+++ DSA_Programs/LC3461_check_string_after_operation.java
@@ -4,7 +4,7 @@ class Solution {
         while(s.length()>2){//hume bass two digit kat hi chahiye isliye while loop
             StringBuilder sb=new StringBuilder();//abhi isme stringBuilder ki jarirat padegi kyuki overall pura number collect karne keliye
             for(int i=0;i<s.length()-1;i++){
-                int y=((s.charAt(i)-'0'+s.charAt(i+1)-'0')%10);
+                int y=((s.charAt(i)-'0'+s.charAt(i+1)-'0')%10);//according to question
                 sb.append(y);
             }
             s=sb.toString();
