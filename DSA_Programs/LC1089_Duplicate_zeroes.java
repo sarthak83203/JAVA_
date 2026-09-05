22012ec2fb5f5f8ffa6bdc63563840a50823dff8
sarthak83203 <sarthak.shinde2006@gmail.com>
2026-09-05 20:50:04 +0530

    Make Duplicate Zeroes in array

---
+++ DSA_Programs/LC1089_Duplicate_zeroes.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Solution {
+    public void duplicateZeros(int[] arr) {
+        ArrayList<Integer> list=new ArrayList<>();
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==0){
+                list.add(arr[i]);    
+            }
+            list.add(arr[i]);
+            
+        }
+
+        for(int i=0;i<arr.length;i++){
+            arr[i]=list.get(i);
+        }
+
+
+       
+      
+
+
+        
+    }
+}
