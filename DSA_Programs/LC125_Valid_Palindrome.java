078250a20e2cc566fe4c0242d02f184438402126
sarthak83203 <sarthak.shinde2006@gmail.com>
2026-09-10 00:59:17 +0530

    Valid Palindrome Easy

--- DSA_Programs/LC125_Valid_Palindrome.java
+++ DSA_Programs/LC125_Valid_Palindrome.java
@@ -2,13 +2,13 @@ class Solution {
     public boolean isPalindrome(String s) {
         StringBuilder sb=new StringBuilder();
         for(int i=0;i<s.length();i++){
-            if(Character.isLowerCase(s.charAt(i))){
+            if(Character.isLowerCase(s.charAt(i))){//this function  should be remember 
                sb.append(s.charAt(i));
             }
-            else if(Character.isUpperCase(s.charAt(i))){
+            else if(Character.isUpperCase(s.charAt(i))){//this function  should be remember 
                 sb.append(Character.toLowerCase(s.charAt(i)));
             }
-            else if(Character.isDigit(s.charAt(i))){
+            else if(Character.isDigit(s.charAt(i))){//this function  should be remember 
                sb.append(s.charAt(i));
             }else{
                 continue;
