5d8631cbcf1c3d215ccb96968e35ff3061fd3e25
sarthak83203 <sarthak.shinde2006@gmail.com>
2026-09-11 22:57:51 +0530

    Next Greater Element with Circular Approach

---
+++ DSA_Programs/LC503_Next_greater_ele_circular.java
@@ -0,0 +1,37 @@
+import java.util.*;
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        //important question for interview 
+        //isme stack ka concept use hoga and Logic is most important
+        //pehele to brute force lagao easy 
+        //Real Interview Approach 
+        //now tum loop backward lagao the main reason behind is that kyuki tum pehele se hi
+        //elements ko pehelese hi analysis kar chuke hoge 
+        //Aur Stack me hamesha index push hoga (main important) kyuki index se track karna easy hota he compare to value
+        
+        //lets go
+
+        Stack<Integer> st=new Stack<>();
+        int greater[]=new int[nums.length];
+        //in question they told circular to circular ke liye ek trick he ki % lelo len ke sath
+        for(int i=2*nums.length-1;i>=0;i--){//circular he to mujhe 1st element ko dekhna padega 
+        int index=i%nums.length;//ye baas arrat me fix karne ke liye kiyya gaya he
+            while(!st.isEmpty() && nums[st.peek()]<=nums[index]){
+                //agar curr element ki value jyada he to nikalo
+                //take the example of [6,8,0,1,3]
+                st.pop();
+            }
+            if(st.isEmpty()){
+                greater[index]=-1;
+            }else{
+                greater[index]=nums[st.peek()];//kyuki stack me element nahi index store he 
+            }
+            st.push(index);//push karo current element ka index
+        }
+        return greater;
+
+
+        
+        
+    }
+}
