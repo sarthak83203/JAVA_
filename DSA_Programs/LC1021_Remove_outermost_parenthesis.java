f0bf9f7adf499fda3008b945bee7c19ade2634dc
sarthak83203 <sarthak.shinde2006@gmail.com>
2026-09-13 12:33:17 +0530

    Nice TagTicks question logic

---
+++ DSA_Programs/LC1021_Remove_outermost_parenthesis.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class Solution {
+    public String removeOuterParentheses(String s) {
+        StringBuilder sb=new StringBuilder();
+        Stack<Character> st=new Stack<>();
+        //isme pehele to dekho ki stack empty he ya bhara hua he 
+        //phir baad me agar size 0> ho to string me append karna phir pehela wala bracket aise hi rah gaya
+        //phir jab closing hoga tab tab pehele nikalna kyuki last wala string me nahi jayega phir
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='('){
+                if(st.size()>0){
+                    sb.append('(');
+                }
+                st.push('(');
+            }else{
+                st.pop();
+                if(st.size()>0){
+                    sb.append(')');
+                }
+            }
+        }
+        return sb.toString();
+
+
+
+       
+        
+        
+    }
+}
