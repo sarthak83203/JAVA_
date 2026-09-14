8c262096daf00943cd105d06c00a9ff696d1b656
sarthak83203 <sarthak.shinde2006@gmail.com>
2026-09-14 13:56:47 +0530

    Build and Array Unique question

--- DSA_Programs/LC1441_Build_array_with_stack_unique.java
+++ DSA_Programs/LC1441_Build_array_with_stack_unique.java
@@ -4,7 +4,7 @@ class Solution {
         ArrayList<String> result=new ArrayList<>();
         int current=1;
         for(int i=0;i<target.length;i++){
-            while(current<target[i]){
+            while(current<target[i]){//agar equal to hoga to direct push  hoga without any condition
                 result.add("Push");
                 result.add("Pop");
                 current++;
