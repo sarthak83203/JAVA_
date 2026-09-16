217624335ee851a0d090a5f877bdb758a95bf555
sarthak83203 <sarthak.shinde2006@gmail.com>
2026-09-16 21:16:00 +0530

    Counting of the vowels starting and ending with vowel

---
+++ DSA_Programs/LC2586_count_number_of_vowel.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int vowelStrings(String[] words, int left, int right) {
+        int count=0;
+        for(int i=left;i<=right;i++){
+            String h=words[i];
+            int len=h.length()-1;
+            if((h.charAt(0)=='a'||h.charAt(0)=='e' || h.charAt(0)=='i' || h.charAt(0)=='o' ||h.charAt(0)=='u') && (h.charAt(len)=='a' || h.charAt(len)=='e' || h.charAt(len)=='i' || h.charAt(len)=='o' || h.charAt(len)=='u')){
+                count++;
+            }
+        }
+        return count;
+
+
+        
+    }
+}
