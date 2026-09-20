9b30f40270b04f30ef910287716f6feffd9c2164
sarthak83203 <sarthak.shinde2006@gmail.com>
2026-09-20 10:07:38 +0530

    Reverse Degree of the string

---
+++ DSA_Programs/LC3498_Reverse_degree_of_string.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int reverseDegree(String s) {
+        int sum=0;
+      
+        for(int i=0;i<s.length();i++){
+           int value='z'-s.charAt(i)+1;
+           sum=sum+(value*(i+1));
+
+        }
+        return sum;
+        
+    }
+}
